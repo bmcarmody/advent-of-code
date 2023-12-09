5f53a96d13a5cbcb9c1826406607c8a8e46184fb
Brandon Carmody <BmcPrimary@yahoo.com>
2023-12-08 23:39:30 -0800

    finish day1 challenge 1

---
+++ day1/1-calibration-sum.py
@@ -0,0 +1,27 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+runningTotal = 0
+
+with open('./input.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+
+        left = 0
+        right = len(line) - 1
+
+        while left <= right:
+            if not line[left].isdigit():
+                left += 1
+
+            if not line[right].isdigit():
+                right -= 1
+
+            if line[left].isdigit() and line[right].isdigit():
+                break
+            
+        concatLeftRight = str(line[left]) + str(line[right])
+        runningTotal += int(concatLeftRight)
+
+logging.info(runningTotal)
